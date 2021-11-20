cab9321b84a01bcfc7d7974c42bfc4aa899938d2
peuks <vanmakdavid.lwg@gmail.com>
2021-11-20 14:05:22 +0100

    INIT: Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { CssBaseline, Grid } from "@material-ui/core";
+import React from "react";
+import { Header, Map, List } from "./components";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header y />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "@/components/Header/Header";
+export { default as List } from "@/components/List/List";
+export { default as Map } from "@/components/Map/Map";
+export { default as PlaceDetails } from "@/components/PlaceDetails/PlaceDetails";
